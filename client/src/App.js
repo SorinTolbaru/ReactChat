e401f18f6cbb435fa03a6d8a9cc43ebddf38701e
SorinTolbaru <tolbarusorin@gmail.com>
2023-12-26 19:22:08 +0200

    added socket.io and established connection between server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
+import { io } from "socket.io-client";
+import { useRef } from "react";
+
 
 function App() {
+  const socket = useRef();
+  socket.current = io(`http://${window.location.hostname}:5000`);
   return (
     <div className="App">
         <div>works</div>
--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const cors = require("cors");
-
+const socketIo = require("socket.io");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+require("dotenv").config();
+
+const server = app.listen(5000, '0.0.0.0', () => {
+  console.log("Server running on port http://localhost:5000");
+
+  const io = socketIo(server, {
+    cors: {
+      origin: "*",
+      credentials: true,
+    },
+  });
+
+  io.on("connection", async (socket) => {
+    console.log("A user connected");
 
 
-const server = app.listen(5000,'0.0.0.0', () => {
-    console.log("Server running on port http://localhost:5000");
-
-})
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
+  });
+});
