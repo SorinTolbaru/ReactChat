fb24ecb43cedbbbb07f8f2c6a23cbc229df2410e
SorinTolbaru <tolbarusorin@gmail.com>
2024-01-07 18:46:00 +0200

    Added mongoose, messages are now saved on Mongodb Atlas

---
+++ server/models/messageSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const chatSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+const Chat = mongoose.model("Chat", chatSchema)
+
+module.exports = Chat
--- server/server.js
+++ server/server.js
@@ -1,78 +1,98 @@
 const express = require("express")
 const cors = require("cors")
 const socketIo = require("socket.io")
+const mongoose = require("mongoose")
+const Chat = require("./models/messageSchema.js")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 require("dotenv").config()
 
-const server = app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on port http://localhost:5000")
+const mongoURL = process.env.MONGO_URL
+mongoose.connect(mongoURL)
 
-  const io = socketIo(server, {
-    cors: {
-      origin: "*",
-      credentials: true,
-    },
-  })
+const dbConnection = mongoose.connection
 
-  const onlineUsers = new Map()
+dbConnection.on("error", (error) => {
+  console.error("MongoDB connection error:", error)
+})
 
-  app.get("/getUsersList", (req, res) => {
-    res.send(Array.from(onlineUsers.values()))
-  })
+dbConnection.once("open", () => {
+  console.log("Connected to MongoDB")
 
-  io.on("connection", async (socket) => {
-    socket.on("enter", (username) => {
-      socket.user = username
-      onlineUsers.set(socket.id, socket.user)
-      io.emit("receive-message", `${socket.user} connected`)
-      console.log(`${socket.user} connected`)
-      console.log(onlineUsers)
+  const server = app.listen(5000, "0.0.0.0", () => {
+    console.log("Server running on port http://localhost:5000")
+
+    const io = socketIo(server, {
+      cors: {
+        origin: "*",
+        credentials: true,
+      },
     })
 
-    socket.on("send-message", (message) => {
-      if (message.includes("/pm ")) {
-        messageAndTo = splitMessage(message, socket.user)
-        let id = Array.from(onlineUsers).find((user) =>
-          user.includes(messageAndTo.to)
-        )
-        if (id && id[0] !== undefined) {
-          socket.to(id[0]).emit("receive-message", messageAndTo.msg)
-          socket.emit("receive-message", messageAndTo.msg)
+    const onlineUsers = new Map()
+
+    app.get("/getUsersList", (req, res) => {
+      res.send(Array.from(onlineUsers.values()))
+    })
+
+    io.on("connection", async (socket) => {
+      socket.on("enter", (username) => {
+        socket.user = username
+        onlineUsers.set(socket.id, socket.user)
+        io.emit("receive-message", `${socket.user} connected`)
+        console.log(`${socket.user} connected`)
+        console.log(onlineUsers)
+      })
+
+      socket.on("send-message", (message) => {
+        if (message.includes("/pm ")) {
+          messageAndTo = splitMessage(message, socket.user)
+          let id = Array.from(onlineUsers).find((user) =>
+            user.includes(messageAndTo.to)
+          )
+          if (id && id[0] !== undefined) {
+            socket.to(id[0]).emit("receive-message", messageAndTo.msg)
+            socket.emit("receive-message", messageAndTo.msg)
+          } else {
+            socket.emit("receive-message", "Server: User can't be found")
+          }
         } else {
-          socket.emit("receive-message", "Server: User can't be found")
+          const messageElement = new Chat({
+            username: socket.user,
+            message: message,
+          })
+          messageElement.save()
+          socket.broadcast.emit("receive-message", `${socket.user}: ${message}`)
+          console.log(`${socket.user}: ${message}`)
         }
-      } else {
-        socket.broadcast.emit("receive-message", `${socket.user}: ${message}`)
-        console.log(`${socket.user}: ${message}`)
+      })
+
+      socket.on("user-typing", (msg) => {
+        io.emit("user-typing", msg)
+      })
+
+      socket.on("disconnect", () => {
+        onlineUsers.delete(socket.id)
+        console.log(`${socket.user} disconnected`)
+        io.emit("receive-message", `${socket.user} disconnected`)
+        console.log(onlineUsers)
+      })
+    })
+
+    function splitMessage(msg, su) {
+      let u
+      let c = msg.slice(0, msg.indexOf(" "))
+      let um = msg.slice(c.length + 1, msg.length)
+      u = um.slice(0, um.indexOf(" "))
+      let m = um.slice(um.indexOf(" "))
+      let M = `${su} > ${u}:${m}`
+      if (u.length == 0 || um.includes(" ") == false) {
+        M = `Invalid pm`
       }
-    })
 
-    socket.on("user-typing", (msg) => {
-      io.emit("user-typing", msg)
-    })
-
-    socket.on("disconnect", () => {
-      onlineUsers.delete(socket.id)
-      console.log(`${socket.user} disconnected`)
-      io.emit("receive-message", `${socket.user} disconnected`)
-      console.log(onlineUsers)
-    })
-  })
-
-  function splitMessage(msg, su) {
-    let u
-    let c = msg.slice(0, msg.indexOf(" "))
-    let um = msg.slice(c.length + 1, msg.length)
-    u = um.slice(0, um.indexOf(" "))
-    let m = um.slice(um.indexOf(" "))
-    let M = `${su} > ${u}:${m}`
-    if (u.length == 0 || um.includes(" ") == false) {
-      M = `Invalid pm`
+      return { msg: M, to: u }
     }
-
-    return { msg: M, to: u }
-  }
+  })
 })
